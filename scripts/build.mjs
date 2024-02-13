dfe890514cb3dfb6a88d4a029a978794daf3ebe1
Fierflame <git@fierflame.com>
2024-02-13 23:11:21 +0800

    环境：调整打包配置

---
+++ scripts/build.mjs
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+import { fileURLToPath } from 'node:url';
+import * as fsPromises from 'node:fs/promises';
+import * as pathFn from 'node:path';
+import { rollup } from 'rollup';
+import terser from '@rollup/plugin-terser';
+import dts from 'rollup-plugin-dts';
+
+const {
+	name, version, description, author, license,
+} = JSON.parse(await fsPromises.readFile('./package.json', 'utf-8'));
+
+
+const root = pathFn.resolve(fileURLToPath(import.meta.url), '../..');
+process.chdir(root);
+/**
+ *
+ * @param {string} p
+ * @returns {string}
+ */
+function getPath(p) {
+	return pathFn.resolve(root, p);
+}
+await fsPromises.copyFile(getPath('LICENSE'), getPath('dist/LICENSE'));
+
+
+const components = await fsPromises.readdir(getPath('src'), 'utf8').then(v => v.filter(v => !v.includes('.')));
+
+await fsPromises.mkdir(getPath('dist'), { recursive: true }).catch(() => { });
+
+const mainExport = components.map(v => `export {default as ${v}} from './${v}.mjs';\n`);
+const typeExport = components.map(v => `export type * from './${v}.mjs';\n`);
+fsPromises.writeFile(getPath('dist/index.mjs'), [...mainExport].join(''));
+fsPromises.writeFile(getPath('dist/index.d.mts'), [...mainExport, ...typeExport].join(''));
+fsPromises.writeFile(getPath('dist/package.json'), JSON.stringify({
+	name, version, description, author, license,
+	main: 'index.mjs', type: 'module',
+	unpkg: './index.min.js', jsdelivr: './index.min.js',
+	exports: {
+		'.': './index.mjs',
+		...Object.fromEntries(components.map(v => [`./${v}.mjs`, `./${v}.mjs`])),
+		...Object.fromEntries(components.map(v => [`./${v}`, `./${v}.mjs`])),
+	},
+}, null, 2));
+
+const bYear = 2023;
+const year = new Date().getFullYear();
+const date = bYear === year ? bYear : `${bYear}-${year}`;
+const banner = `\
+/*!
+ * 匿龙组件库 @nyloong/components v${version}
+ * (c) ${date} ${author}
+ * @license ${license}
+ */`;
+
+const readmeFile = await fsPromises.open(getPath(`dist/README.md`), 'w');
+await readmeFile.write('匿龙组件库 @Nyloong/components\n==============================\n');
+
+for (const name of components) {
+	await readmeFile.write('\n');
+	await readmeFile.write(await fsPromises.readFile(getPath(`src/${name}/README.md`)));
+}
+for (const name of components) {
+	const bundle = await rollup({ input: getPath(`src/${name}/index`) });
+	await bundle.write({ banner, file:  getPath(`dist/${name}.mjs`), format: 'es' });
+}
+for (const name of components) {
+	const bundle = await rollup({ input: getPath(`types/${name}/index.d.mts`), plugins: [dts()] });
+	await bundle.write({ banner, file:  getPath(`dist/${name}.d.mts`), format: 'es' });
+}
+
+const input = '\0NyLoongComponents';
+const indexBundle = await rollup({
+	input,
+	plugins: [{
+		name: 'NyLoongComponents',
+		resolveId(source) {
+			if (source !== input) { return null; }
+			return { id: input, moduleSideEffects: true };
+		},
+		load(id) {
+			if (id !== input) { return null; }
+			return components.map(v => `export {default as ${v}} from ${
+				JSON.stringify(getPath(`dist/${v}.mjs`))
+			};\n`).join('');
+		},
+	}],
+});
+await indexBundle.write({ file: getPath('dist/index.min.mjs'), format: 'es', plugins: [terser()], sourcemap: true });
+await indexBundle.write({ file: getPath('dist/index.min.js'), name: 'NyLoongComponents', format: 'umd', plugins: [terser()], sourcemap: true });
+await indexBundle.write({ file: getPath('dist/index.js'), name: 'NyLoongComponents', format: 'umd', sourcemap: true });
--- src/Movable/README.md
+++ src/Movable/README.md
@@ -1,50 +1,7 @@
-匿龙组件库 @Nyloong/components
-==============================
-
-面包屑 `<nl-breadcrumb>`
-------------------------
-
-### 属性
-
-| HTML属性    | JS 属性     | 等价自定义属性              | 类型     | 默认值 | 说明           |
-| ----------- | ----------- | --------------------------- | -------- | ------ | -------------- |
-| `separator` | `separator` | `--nl-breadcrumb-separator` | `string` | ` / `  | 面包屑的分隔符 |
-
-### 实例
-
-```html
-<nl-breadcrumb>
-	<span><a href="#">带链接1</a></span>
-	<span>普通2</span>
-	<span><a href="#">带链接3</a></span>
-	<span>普通4</span>
-	<span>最后一项5</span>
-</nl-breadcrumb>
-```
-
-
-
-角度输入 `<nl-angle>`
-------------------------
-
-角度输入 `<nl-angle>` 是一个 HTML 是输入，支持基本的输入组件属性
-
-
-### 实例
-
-```html
-<form>
-	<nl-angle name="angle" value="30" style="font-size: 5em;"></nl-angle>
-	<button>提交</button>
-</form>
-```
-
-
 可移动区域 `<nl-movable>` `<nl-movable-area>` `<nl-movable-target>`
 ------------------------
 
 `<nl-movable>` 可以在 `<nl-movable-area>` 内进行移动、缩放，如果 `<nl-movable>` 的后代中包含 `<nl-movable-target>`，则只有在作用于 `<nl-movable-target>` 上时，才能移动、缩放
-
 
 ### `<nl-movable>` 属性
 
@@ -74,27 +31,3 @@
 	roller-movable roller-scalable left-mouse-movable></nl-movable>
 </nl-movable-area>
 ```
-
-滚动数字 `<nl-roll-digit>`
-------------------------
-
-### 属性
-
-| HTML属性    | JS 属性     | 类型     | 默认值 | 说明           |
-| ----------- | ----------- | -------- | ------ | -------------- |
-| `digit` | `digit` | `number` | `0`  | 要显示的数字 |
-
-### 实例
-
-```html
-<nl-roll-digit digit="5" style="
-	background-color: #88F;
-	line-height: 2em;
-	inline-size: .8em;
-	border-radius: .2em;
-"></nl-roll-digit>
-```
-
-### 其他
-
-- 不支持 CSS `writing-mode` 属性
