94fc2f883598228fb2eeda1b5f771ec7c4c5834e
Fierflame <git@fierflame.com>
2024-08-06 21:53:02 +0800

    新增：实现 <nl-collapse>

---
+++ src/Collapse/README.md
@@ -0,0 +1,37 @@
+折叠面板 `<nl-collapse>`
+------------------------
+
+支持多层嵌套
+
+### 属性
+
+| HTML属性    | JS 属性     | 类型     | 默认值 | 说明           |
+| ----------- | ----------- | -------- | -------- | -------------- |
+| `nonTrigger` | `nonTrigger`  | `boolean` | `false` | 是否隐藏切换器 |
+| `startTrigger` | `startTrigger`  | `boolean` | `false` | 切换器位置是否在标签之前 |
+| `accordion` | `accordion`  | `boolean` | `false` | 是否为手风琴模式 |
+| `disabled` | `disabled`  | `boolean` | `false` | 是否禁用 |
+| `open` | `open`  | `boolean` | `false` | 是否打开 |
+| `animation` | `animation`  | `boolean` | `false` | 是否启用动画 |
+| `menu` | `menu`  | `boolean` | `false` | 是否为菜单 |
+| `label` | `label` | `string` | `` | 标签内容 |
+
+### 插槽
+
+| 插槽       | 说明                 |
+| ---------- | ---------------- |
+| _默认插槽_ | 主体内容         |
+| `label`    | 自定义标签内容   |
+| `trigger`  | 自定义切换器图标 |
+
+### 实例
+
+```html
+<nl-collapse animation accordion>
+	<div slot="label">label<button>111</button></div>
+	<div style="background-color:antiquewhite">1</div>
+	<div style="background-color:burlywood">1</div>
+	<div style="background-color:aqua">1</div>
+	<div style="background-color:chartreuse">1</div>
+</nl-collapse>
+```
---
+++ src/Collapse/index.mjs
@@ -0,0 +1,273 @@
+const style = `
+:host {
+	display: block;
+	border: 1px solid #666;
+	writing-mode: inherit;
+}
+:host[hidden] {
+	display: none;
+}
+header {
+	display: flex;
+	border-block-end: inherit;
+	cursor: pointer;
+}
+.label {
+	flex: 1;
+	display: flex;
+}
+.trigger {
+	width: .35em;
+	height: .35em;
+	border: .15em solid currentcolor;
+	border-block-start: none;
+	border-inline-start: none;
+	margin-inline: .3em .5em;
+	margin-block: auto;
+	transform: rotate(-45deg);
+}
+section {
+	overflow: hidden;
+}
+main {
+	padding: 8px;
+}
+
+:host(:not([open])) section {
+	block-size: 0!important;
+}
+:host(:not([open])) header {
+	margin-block-end: -1px;
+}
+
+:host([open]) .trigger {
+	transform: rotate(45deg);
+}
+
+
+:host([menu]) {
+		border: none;
+}
+:host([menu]) main {
+	padding: 0!important;
+}
+:host([menu]) header {
+	margin-block-end: 0;
+}
+:host([animation]) section {
+	transition: block-size .3s;
+}
+:host([animation]) .trigger {
+	transition: transform .3s;
+}
+:host([non-trigger]) .trigger-slot {
+	display: none;
+}
+
+:host([start-trigger]) header {
+	flex-direction: row-reverse;
+}
+`;
+
+const verticalWritingMode = new Set([
+	'vertical-lr', 'vertical-rl', 'sideways-lr', 'sideways-rl',
+]);
+/**
+ *
+ * @param {HTMLElement} el
+ * @param {string} name
+ * @param {boolean} value
+ */
+function setBoolAttribute(el, name, value) {
+	if (value) {
+		el.setAttribute(name, '');
+	} else {
+		el.removeAttribute(name);
+	}
+}
+export default class Collapse extends HTMLElement {
+	static observedAttributes = ['accordion', 'disabled', 'open', 'menu', 'animation', 'label'];
+	/** @type {boolean} 无折叠图标 */
+	get nonTrigger() { return this.getAttribute('non-trigger') !== null; }
+	set nonTrigger(value) { setBoolAttribute(this, 'non-trigger', value); }
+	/** @type {boolean} 图标在开头 */
+	get startTrigger() { return this.getAttribute('start-trigger') !== null; }
+	set startTrigger(value) { setBoolAttribute(this, 'start-trigger', value); }
+	/** @type {boolean} 手风琴 */
+	get accordion() { return this.getAttribute('accordion') !== null; }
+	set accordion(value) { setBoolAttribute(this, 'accordion', value); }
+	/** @type {boolean} 禁用 */
+	get disabled() { return this.getAttribute('disabled') !== null; }
+	set disabled(value) { setBoolAttribute(this, 'disabled', value); }
+	/** @type {boolean} 打开状态 */
+	get open() { return this.getAttribute('open') !== null; }
+	set open(value) { setBoolAttribute(this, 'open', value); }
+	/** @type {boolean} 动画 */
+	get animation() { return this.getAttribute('animation') !== null; }
+	set animation(value) { setBoolAttribute(this, 'animation', value); }
+	/** @type {boolean} 菜单模式 */
+	get menu() { return this.getAttribute('menu') !== null; }
+	set menu(value) { setBoolAttribute(this, 'menu', value); }
+	/** @type {boolean} 标签 */
+	get label() { return this.getAttribute('label') || ''; }
+	set label(value) {
+		if (value) {
+			this.setAttribute('label', value);
+		} else {
+			this.removeAttribute('label');
+		}
+	}
+
+
+	#shadow = (() => {
+		const shadow = this.attachShadow({mode:'closed'});
+		shadow.appendChild(document.createElement('style')).textContent = style;
+		return shadow;
+	})();
+	#header = (() => {
+		const header = document.createElement('header');
+		this.#shadow.appendChild(header);
+		header.tabIndex = 0;
+		/** @param {Event} e */
+		const click = e => {
+			if (this.disabled) { return; }
+			if (this.#shadow.activeElement !== header) { return; }
+			if (!this.dispatchEvent(new Event('beforechange', {cancelable: true}))) { return; }
+			this.open = !this.open;
+			this.dispatchEvent(new Event('change'));
+		};
+		header.addEventListener('click', click);
+		header.addEventListener('keydown', e => {
+			if (e.code === 'Enter' || e.code === 'Space') {
+				click(e);
+			}
+		});
+		return header;
+	})();
+	#label = (() => {
+		const slot = document.createElement('slot');
+		this.#header.appendChild(slot);
+		slot.className = 'label';
+		slot.name = 'label';
+		return slot;
+	})();
+	#trigger = (() => {
+		const slot = document.createElement('slot');
+		this.#header.appendChild(slot);
+		slot.name = 'trigger';
+		slot.className = 'trigger-slot';
+		slot.appendChild(document.createElement('span')).className = 'trigger';
+		return slot;
+	})();
+
+	#body = (() => {
+		const body = document.createElement('section');
+		this.#shadow.appendChild(body);
+		return body;
+	})();
+	#main = (() => {
+		const main = document.createElement('main');
+		this.#body.appendChild(main);
+		main.appendChild(document.createElement('slot'));
+		return main;
+	})();
+
+
+	#updateSize() {
+		const vertical = verticalWritingMode.has(getComputedStyle(this).writingMode?.toLowerCase());
+		this.#body.style.blockSize = `${vertical ? this.#main.clientWidth : this.#main.clientHeight}px`;
+
+	}
+	updateSize() { this.#updateSize(); }
+	/** @type {ResizeObserver?} */
+	#ro = new ResizeObserver(() => this.#updateSize());
+	/**
+	 * @returns {void}
+	 */
+	connectedCallback() {
+		this.#ro.observe(this.#main);
+	}
+	/**
+	 * @returns {void}
+	 */
+	disconnectedCallback() {
+		this.#ro.unobserve(this.#main);
+	}
+	/**
+	 * @returns {void}
+	 */
+	adoptedCallback() { }
+	#closeOthersWhenOpen() {
+		const parent = this.parentElement;
+		if (!parent) { return; }
+		const style = getComputedStyle(parent).whiteSpace?.toLowerCase() || '';
+		const pre = style.startsWith('pre') || style === 'break-spaces';
+		/** @type {Node?} */
+		let node = this;
+		// eslint-disable-next-line no-cond-assign
+		while (node = node.previousSibling) {
+			if (node instanceof Comment) { continue; }
+			if (node instanceof Text) {
+				const text = node.textContent || '';
+				if (pre ? text.includes('\n') : text.replace(/[\s\n ]/g, '')) { break; }
+				continue;
+			}
+			if (!(node instanceof Collapse)) { break; }
+			if (!node.accordion) { break; }
+			node.open = false;
+		}
+		node = this;
+		// eslint-disable-next-line no-cond-assign
+		while (node = node.nextSibling) {
+			if (node instanceof Comment) { continue; }
+			if (node instanceof Text) {
+				const text = node.textContent || '';
+				if (pre ? text.includes('\n') : text.replace(/[\s\n ]/g, '')) { break; }
+				continue;
+			}
+			if (!(node instanceof Collapse)) { break; }
+			if (!node.accordion) { break; }
+			node.open = false;
+		}
+	}
+	/**
+	 * @param {string} attrName
+	 * @param {string | null} oldVal
+	 * @param {string | null} newVal
+	 * @returns {void}
+	 * @protected
+	 */
+	attributeChangedCallback(attrName, oldVal, newVal) {
+		if (oldVal === newVal) { return; }
+		switch (attrName) {
+			case 'accordion': {
+				if (newVal === null) { break; }
+				if (!this.open) { break; }
+				this.#closeOthersWhenOpen();
+				break;
+			}
+			case 'disabled': {
+				if (newVal === null) {
+					this.#header.tabIndex = 0;
+				} else {
+					this.#header.removeAttribute('tabindex');
+				}
+				break;
+			}
+			case 'open': {
+				if (!this.accordion) { break; }
+				if (!this.open) { break; }
+				this.#closeOthersWhenOpen();
+				break;
+			}
+			case 'label': {
+				this.#label.innerText = newVal || '';
+				break;
+			}
+		}
+	}
+}
+
+try {
+	customElements.define('nl-collapse', Collapse);
+} catch {}
